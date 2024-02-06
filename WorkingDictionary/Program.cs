adb06fb6288165c67d87a17fcd82fd744f6c50bd
Zeds_Xd <homenkor24@gmail.com>
2024-02-06 12:22:13 +0300

    Dictionary

---
+++ WorkingDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanCSharp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var userData = GetUserData();
+            Console.WriteLine($"Здравствуйте: {userData["Name"]}, Ваш возраст: {userData["Age"]}, Ваш пол: {userData["Sex"]}");
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, string> GetUserData()
+        {
+            var outDictionary = new Dictionary<string, string>();
+            Console.WriteLine("Введите свои данные: ");
+
+            outDictionary.Add("Name", Console.ReadLine());
+            outDictionary.Add("Age", Console.ReadLine());
+            outDictionary.Add("Sex", Console.ReadLine());
+
+            return outDictionary;
+        }
+    }
+}
